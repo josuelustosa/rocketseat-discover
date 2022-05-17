241e29d7923834c8dc6e52d06d38842765d3655b
Josué Lustosa <josue.filho@conecthus.org.br>
2022-05-17 19:04:28 -0300

    :construction: styling css page

--- especializar/fundamentos-react/reactapp/src/pages/Home/index.jsx
+++ especializar/fundamentos-react/reactapp/src/pages/Home/index.jsx
@@ -2,10 +2,10 @@ import './style.css'
 
 export function Home() {
   return (
-    <div>
-    <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite o nome" />
-    <button type="button">Adicionar</button>
+    <div className="container">
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome" />
+      <button type="button">Adicionar</button>
     </div>
   )
 }
